7fca0951f2fe27756d59407e974ca5ef598a940d
nick stanton <nick.a.stanton@gmail.com>
2016-02-08 14:17:55 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+var express       = require('express'),
+    bodyParser    = require('body-parser'),
+    mongoose      = require('mongoose');
+
+
+var app = express();
+app.use(express.static('./public'));
+app.use( bodyParser.urlencoded({extended: true}) );
+app.use( bodyParser.json() );
+var mongoPath = 'mongodb://localhost/outhouse-app-01';
+mongoose.connect(process.env.MONGOLAB_URI||mongoPath);
+
+
+
+var users = require('./routers/users');
+app.use('/api/users', users);
+var posts = require('./routers/outhouses');
+app.use('/api/outhouses', outhouses);
+
+app.get('/', function(req, res){
+ res.sendFile( __dirname + '/views/index.html');
+})
+
+var port = 8080;
+app.listen(process.env.PORT || port, function(){
+  console.log('...listening on ' + port);
+});
