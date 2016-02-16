d7ff70ea14a54629fb29f23a1563e4aa5f5a3ec5
nick stanton <nick.a.stanton@gmail.com>
2016-02-16 11:04:25 -0500

    debugged... for now

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -9,17 +9,15 @@ ctlr.controller('mapController', ['$scope','outhousesApi','$cookies','tokenServi
     $scope.rating.username = $scope.outhouse.poster;
     $scope.rating.value = parseInt(rating);
     outhouse.ratings[0] = $scope.rating;
-    // console.log(outhouse.ratings[0]);
-    // console.log('1');
-     outhouse.position = {};
+    outhouse.position = {};
     outhousesApi.addressLatLng(outhouse).then(function (addressResponse) {
-      // console.log('2');
       outhouse.position.lat = addressResponse.data.results[0].geometry.location.lat;
       outhouse.position.lng = addressResponse.data.results[0].geometry.location.lng;
         outhousesApi.createNew(outhouse).then(function (response) {
           console.log(response.config.data.outhouse);
           $scope.outhouses.push(response.config.data.outhouse)
-          $scope.outhouse = {};
+          $rootScope.outhouse = {};
+          $rootScope.rating=0;
           $rootScope.showForm = false;
           $scope.addMarker(response.config.data.outhouse);
         })
@@ -30,13 +28,11 @@ ctlr.controller('mapController', ['$scope','outhousesApi','$cookies','tokenServi
     for (var i = 0; i < outhouse.ratings.length; i++) {
       total += outhouse.ratings[i].value
     }
-    var averageRating = (total/outhouse.ratings.length).toFixed(2);
-     console.log(averageRating);
+    var averageRating = (total/i).toFixed(2);
     return averageRating;
   }
   $scope.checkRating = function (outhouse) {
     var rated = false;
-    // console.log('rating');
     outhouse.ratings.forEach( function (rating) {
       if(rating.username == $rootScope.currentUser){
         rated = true;
@@ -48,19 +44,17 @@ ctlr.controller('mapController', ['$scope','outhousesApi','$cookies','tokenServi
     return rated;
   }
   $scope.addRating = function(outhouse, rating){
-    console.log('addRating');
-    console.log(rating);
-    console.log(outhouse);
-    var value = rating || 3;
+    var value = parseInt(rating) || 3;
     outhouse.ratings.push({username: $rootScope.currentUser, value: value});
     outhousesApi.updateOuthouse(outhouse).then( function(response) {
      console.log(response);
+     $scope.userRating = value;
+     $scope.selectedOuthouse.average = $scope.average($scope.selectedOuthouse)
      $scope.rated = true;
+
     })
   }
   $scope.infoContent = function(outhouse){
-
-    // console.log(outhouse);
     var infoString = '<div class="infoContent"><h3>' + outhouse.title + '</h3>'+
     '<form ng-hide="rated||!currentUser" ng-submit="addRating(selectedOuthouse, rating)">'+
     '<div class="stars2"><strong>Add your rating</strong><br>'+
@@ -75,42 +69,35 @@ ctlr.controller('mapController', ['$scope','outhousesApi','$cookies','tokenServi
     '<input class="star star-1" id="star-1" type="radio" ng-model="rating" name="star" value="1"/>'+
     '<label class="star star-1" for="star-1"></label><br>'+
     '</div><br><input class="btn-danger" type="submit" name="name"></input>'+
-    '</form><br><h4>' + $scope.average(outhouse) + ' average out of 5 stars </h4>'+
-    '<h4 ng-show="rated">You rated this one ' + $scope.userRating + ' stars.</h4>'+
+    '</form><br><h4>{{selectedOuthouse.average}} average out of 5 stars </h4>'+
+    '<h4 ng-show="rated">You rated this one {{userRating}} stars.</h4>'+
     '<br><h6>' + outhouse.description + '</h6></div>'
     return infoString;
   }
 
 
-$scope.markerWindows = function (marker, outhouse) {
-  $scope.rated = $scope.checkRating(outhouse);
+  $scope.markerWindows = function (marker, outhouse) {
+    $scope.rated = $scope.checkRating(outhouse);
 
 
-  google.maps.event.addListener(marker, 'click', function() {
-    $scope.selectedOuthouse = outhouse;
-    $scope.rating='';
-    console.log($scope.selectedOuthouse);
-    console.log($scope.checkRating($scope.selectedOuthouse));
-    $scope.rated = $scope.checkRating($scope.selectedOuthouse);
-    console.log($scope.rated);
-        if( prev_infowindow ) {
-           prev_infowindow.close();
-        }
-
-        marker.info = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'click', function() {
+      $scope.selectedOuthouse = outhouse;
+      $scope.selectedOuthouse.average = $scope.average($scope.selectedOuthouse)
+      $scope.rated = $scope.checkRating($scope.selectedOuthouse);
+      if( prev_infowindow ) {
+       prev_infowindow.close();
+      }
+      marker.info = new google.maps.InfoWindow();
         var content = $scope.infoContent(outhouse)
-        var compiled = $compile(content)($scope);
+          var compiled = $compile(content)($scope);
+            marker.info.setContent( compiled[0] );
+      prev_infowindow = marker.info;
+      marker.info.open(myMap.map, marker);
+    });
 
-        marker.info.setContent( compiled[0] );
-        // $compile(marker.info.content)($scope);
-        prev_infowindow = marker.info;
-        marker.info.open(myMap.map, marker);
-  });
-
-}
+  }
 
   $scope.addMarker = function (outhouse) {
-    // console.log(outhouse);
     var image = '/images/outhouse.png';
     var marker = new google.maps.Marker({
         map: myMap.map,
@@ -129,8 +116,8 @@ $scope.markerWindows = function (marker, outhouse) {
       })
     })
   }
+
   $scope.toggleForm = function () {
-  // console.log('dude');
     $rootScope.showForm = !$rootScope.showForm;
     $scope.outhouse = {};
     var formDiv = document.getElementById('outhouseFormHolder');
@@ -139,10 +126,11 @@ $scope.markerWindows = function (marker, outhouse) {
 
   var prev_infowindow = false;
   var myMap = {};
+
   $rootScope.initMap = function () {
     myMap.init();
-
   }
+
   myMap.init = function() {
       var mapScope = this;
       prev_infowindow = false;
@@ -158,7 +146,6 @@ $scope.markerWindows = function (marker, outhouse) {
       })
 
       this.initialize = function (pos) {
-
         mapScope.currentLatLng = new google.maps.LatLng(pos.latitude, pos.longitude);
         mapScope.map = new google.maps.Map(mapScope.mapEl, {
           center: mapScope.currentLatLng,
